0fcabadcdafd380c017ea66e79ee4ce3539497ec
randomNerd14 <btech10404.21@bitmesra.ac.in>
2022-12-22 00:36:08 +0530

    Added Footer file

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export const Footer = () => {
+  let footerStyle = {
+    position: "absolute",
+    top:"100vh",
+    width: "100%"
+  }
+
   return (
-    <div>Footer Works!</div>
+    <footer className="bg-dark text-light" style={footerStyle}>
+      <p className="text-center">
+        Copyright &copy; MyTodosList.com
+      </p>
+    </footer>
   )
 }
