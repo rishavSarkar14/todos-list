f6655b2834e57b4b40c7e4c5fccb968d84d797cb
randomNerd14 <btech10404.21@bitmesra.ac.in>
2022-12-22 00:36:41 +0530

    Added functionalities

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -4,12 +4,13 @@ import {TodoItem} from "./TodoItem";
 export const Todos = (props) => {
   return (
     <div className="container">
-      <h3 className="text-center my-3">Todos List</h3>
-      {props.todos.map((todo) => {
-        return (
-        <>
-          <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
-        </>
+      <h3 className="my-3">Todos List</h3>
+      {props.todos.length===0? "No Todos to display": 
+        props.todos.map((todo) => {
+         return (
+          <>
+            <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+          </>
       )})}
         
     </div>
