3f71e197b2ca1ccf4eec714e8336414ed7ac055a
randomNerd14 <btech10404.21@bitmesra.ac.in>
2022-12-21 19:29:20 +0530

    Added Todos array and a function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,38 @@ import Header from './MyComponents/Header';
 import {Todos} from "./MyComponents/Todos";
 import {Footer} from "./MyComponents/Footer";
 function App() {
+
+  const onDelete = (todo)=>{console.log("I am onDelete", todo);}
+
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get the job done"
+    },
+
+    {
+      sno: 2,
+      title: "Go to the market",
+      desc: "You need to go to the market to get the job done"
+    },
+
+    {
+      sno: 3,
+      title: "Go to the market",
+      desc: "You need to go to the market to get the job done"
+    },
+
+    {
+      sno: 4,
+      title: "Go to the market",
+      desc: "You need to go to the market to get the job done"
+    }
+  ]
   return (
     <>
-      <Header/>
-      <Todos/>
+    <Header title="My Todos List" searchBar={true}/>
+      <Todos todos={todos} onDelete={onDelete}/>
       <Footer/>
     </>
   );
