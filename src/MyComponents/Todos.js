0d068f02c09130d01a378960e5ab614641edd88c
randomNerd14 <btech10404.21@bitmesra.ac.in>
2022-12-21 19:29:49 +0530

    Added further changes

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {TodoItem} from "./TodoItem";
 
-export const Todos = () => {
+export const Todos = (props) => {
   return (
-    <div>Todos works!</div>
+    <div className="container">
+      <h3 className="text-center my-3">Todos List</h3>
+      {props.todos.map((todo) => {
+        return (
+        <>
+          <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+        </>
+      )})}
+        
+    </div>
   )
 }
