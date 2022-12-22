f0fc47c9b8aef4b0320845fd05046c148dd7c90c
randomNerd14 <btech10404.21@bitmesra.ac.in>
2022-12-22 19:12:34 +0530

    About section of the page

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at pharetra tellus, ut fringilla diam. Cras scelerisque nisl id tincidunt bibendum. Ut posuere, orci ut tincidunt facilisis, enim lectus vestibulum tellus, id porttitor mi felis quis eros. Nam ornare sit amet enim vitae ultricies. Sed interdum dui vitae tortor eleifend, vel convallis mauris tincidunt. Proin pellentesque mauris hendrerit quam rutrum, sit amet vehicula dolor porta. Etiam turpis urna, rhoncus ac consequat quis, condimentum sed massa. Donec luctus porttitor massa, non suscipit sem pharetra id. Aliquam et dui lacus. Phasellus pretium erat tellus, a vestibulum odio tincidunt eu. Sed vel ex posuere, fringilla justo ut, varius est. Vivamus sit amet ultricies ipsum. Integer interdum ex eget nulla varius, elementum facilisis odio dictum. Nulla sed sem pharetra, commodo magna in, scelerisque leo.
+        </p>
+    </div>
+  )
+}
